07123952f255c8aee635034bcba0fb0974baf113
PavelYurtaev <yurtaev.ps@gmail.com>
2018-06-11 15:57:41 +0300

    exception handling in AppointmentService

--- src/main/java/com/yurtaevpavel/appointments/services/AppointmentService.java
+++ src/main/java/com/yurtaevpavel/appointments/services/AppointmentService.java
@@ -1,7 +1,13 @@
 package com.yurtaevpavel.appointments.services;
 
+import com.yurtaevpavel.appointments.model.AppointmentStatus;
+import com.yurtaevpavel.appointments.model.dto.AppointmentDto;
+import com.yurtaevpavel.appointments.model.dto.NoteDto;
 import com.yurtaevpavel.appointments.model.entities.Appointment;
+import com.yurtaevpavel.appointments.model.entities.Company;
+import com.yurtaevpavel.appointments.model.exceptions.AppointmentServiceException;
 import com.yurtaevpavel.appointments.model.repositories.AppointmentRepository;
+import com.yurtaevpavel.appointments.model.repositories.CompaniesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,14 +17,57 @@ import java.util.List;
 public class AppointmentService {
 
     private AppointmentRepository appointmentRepository;
+    private CompaniesRepository companiesRepository;
 
     @Autowired
-    public AppointmentService(AppointmentRepository appointmentRepository) {
+    public AppointmentService(AppointmentRepository appointmentRepository, CompaniesRepository companiesRepository) {
         this.appointmentRepository = appointmentRepository;
+        this.companiesRepository = companiesRepository;
     }
 
     public List<Appointment> getAllAppointments() {
         return appointmentRepository.findAll();
+    }
+
+    public void deleteAppointment(Long id) {
+        appointmentRepository.deleteById(id);
+    } // TODO exception handling
+
+    public Appointment getAppointmentById(Long id) {
+        return getAppointmentOrException(id);
+    }
+
+    public void saveAppointment(AppointmentDto newAppointment) {
+        Long companyId = newAppointment.getCompanyId();
+        Company company = companiesRepository.findById(companyId)
+                .orElseThrow(() -> new AppointmentServiceException(
+                        String.format("Company with id=%s does not exist", companyId)));
+        Appointment appointment = new Appointment(newAppointment.getDate(), company, AppointmentStatus.NEW);
+        appointmentRepository.save(appointment);
+    }
+
+    public void confirmAppointment(Long id) {
+        Appointment appointment = getAppointmentOrException(id);
+        appointment.setStatus(AppointmentStatus.CONFIRMED);
+        appointmentRepository.save(appointment);
+    }
+
+    public void setAppointmentNote(NoteDto note) {
+        Appointment appointment = getAppointmentOrException(note.getAppointmentId());
+        appointment.setNote(note.getNote());
+        appointmentRepository.save(appointment);
+    }
+
+    public void cancelAppointment(Long id) {
+        Appointment appointment = getAppointmentOrException(id);
+        appointment.setStatus(AppointmentStatus.CANCELLED);
+        appointmentRepository.save(appointment);
+    }
+
+    private Appointment getAppointmentOrException(Long id) {
+        return appointmentRepository.findById(id)
+                .orElseThrow(() -> new AppointmentServiceException(
+                        String.format("Appointment with id=%s does not exist", id)));
     }
 
 }
