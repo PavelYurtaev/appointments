7d4825a0329ac6114ab21edcf0d68a9891ea8c60
PavelYurtaev <yurtaev.ps@gmail.com>
2018-06-11 15:56:58 +0300

    add companies service

--- src/main/java/com/yurtaevpavel/appointments/services/CompaniesService.java
+++ src/main/java/com/yurtaevpavel/appointments/services/CompaniesService.java
@@ -1,4 +1,23 @@
 package com.yurtaevpavel.appointments.services;
 
+import com.yurtaevpavel.appointments.model.entities.Company;
+import com.yurtaevpavel.appointments.model.repositories.CompaniesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CompaniesService {
+
+    private CompaniesRepository companiesRepository;
+
+    @Autowired
+    public CompaniesService(CompaniesRepository companiesRepository) {
+        this.companiesRepository = companiesRepository;
+    }
+
+    public List<Company> getAllCompanies() {
+        return companiesRepository.findAll();
+    }
 }
