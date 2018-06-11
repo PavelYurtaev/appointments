31e3b3de932768c8c20e1af893533f16fa6cf0cb
PavelYurtaev <yurtaev.ps@gmail.com>
2018-06-11 15:54:08 +0300

    add request DTOs

--- src/main/java/com/yurtaevpavel/appointments/model/dto/AppointmentDto.java
+++ src/main/java/com/yurtaevpavel/appointments/model/dto/AppointmentDto.java
@@ -1,4 +1,35 @@
 package com.yurtaevpavel.appointments.model.dto;
 
+import java.time.LocalDateTime;
+
 public class AppointmentDto {
+
+    private Long id;
+    private LocalDateTime date;
+    private Long companyId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Long getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Long companyId) {
+        this.companyId = companyId;
+    }
+
 }
--- src/main/java/com/yurtaevpavel/appointments/model/dto/NoteDto.java
+++ src/main/java/com/yurtaevpavel/appointments/model/dto/NoteDto.java
@@ -1,4 +1,23 @@
 package com.yurtaevpavel.appointments.model.dto;
 
 public class NoteDto {
+
+    private Long appointmentId;
+    private String note;
+
+    public Long getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(Long appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
