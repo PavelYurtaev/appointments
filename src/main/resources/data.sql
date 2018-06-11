ebd2698389c844071cca07936b1c1614986294cf
PavelYurtaev <yurtaev.ps@gmail.com>
2018-06-11 15:59:31 +0300

    fix db data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,7 @@ INSERT INTO Location VALUES (2, 'Linconln st', '76', 'San Francisco', '2342451')
 
 INSERT INTO Company VALUES (0, 'Google', 2, 'google.com', 1);
 INSERT INTO Company VALUES (1, 'Apple', 0, 'apple.com', 0);
-INSERT INTO Company VALUES (2, 'Microsoft st', 1, 'microsoft.com', 2);
+INSERT INTO Company VALUES (2, 'Microsoft', 1, 'microsoft.com', 2);
 
 INSERT INTO Appointment VALUES (0, '2018-10-09 10:30:22', 2, '', 'NEW');
 INSERT INTO Appointment VALUES (1, '2018-08-07 20:40:52', 0, '', 'NEW');
